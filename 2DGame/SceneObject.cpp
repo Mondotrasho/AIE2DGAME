4fd21554663ed9b6c3fbea2e235a8af137844aed
Mondotrasho <oscar@peoplepictures.com.au>
2018-07-16 19:28:45 +1000

    Created scene object

---
+++ 2DGame/SceneObject.cpp
@@ -0,0 +1,81 @@
+#include "SceneObject.h"
+#include <cassert>
+
+
+SceneObject::SceneObject()
+{
+}
+
+void SceneObject::addChild(SceneObject* child) {
+	// make sure it doesn't have a parent already
+	assert(child->m_parent == nullptr);
+	// assign "this as parent
+	child->m_parent = this;
+	// add new child to collection
+	m_children.push_back(child);
+}
+
+void SceneObject::removeChild(SceneObject* child) {
+	// find the child in the collection
+	auto iter = std::find(m_children.begin(),
+		m_children.end(), child);
+	// if found, remove it
+	if (iter != m_children.end()) {
+		m_children.erase(iter);
+		// also unassign parent
+		child->m_parent = nullptr;
+	}
+}
+
+
+void SceneObject::update(float deltaTime) {
+	// run onUpdate behaviour
+	onUpdate(deltaTime);
+	// update children
+	for (auto child : m_children)
+		child->update(deltaTime);
+}
+void SceneObject::draw(aie::Renderer2D* renderer) {
+	// run onDraw behaviour
+	onDraw(renderer);
+	// draw children
+	for (auto child : m_children)
+		child->draw(renderer);
+}
+const Matrix3& SceneObject::getLocalTransform() const {
+	return m_localTransform;
+}
+const Matrix3& SceneObject::getGlobalTransform() const {
+	return m_globalTransform;
+}void SceneObject::updateTransform() {
+	if (m_parent != nullptr)
+		m_globalTransform = m_parent->m_globalTransform *
+		m_localTransform;
+	else
+		m_globalTransform = m_localTransform;
+	for (auto child : m_children)
+		child->updateTransform();}
+void SceneObject::setPosition(float x, float y) {
+	m_localTransform[2] = { x, y, 1 };
+	updateTransform();
+}
+void SceneObject::setRotate(float radians) {
+	m_localTransform.set_rotate_z(radians);
+	updateTransform();
+}
+void SceneObject::setScale(float width, float height) {
+	m_localTransform.set_scaled(width, height);
+	updateTransform();
+}
+void SceneObject::translate(float x, float y) {
+	m_localTransform.translate(x, y);
+	updateTransform();
+}
+void SceneObject::rotate(float radians) {
+	m_localTransform.rotate_z(radians);
+	updateTransform();
+}
+void SceneObject::scale(float width, float height) {
+	m_localTransform.scale(width, height);
+	updateTransform();
+}
