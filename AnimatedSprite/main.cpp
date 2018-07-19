d888458ea22860c72d19176b10271e717e2f0dc0
Mondotrasho <oscar@peoplepictures.com.au>
2018-07-19 19:28:05 +1000

    remove memory leak code

--- AnimatedSprite/main.cpp
+++ AnimatedSprite/main.cpp
@@ -1,23 +1,17 @@
 #include "AnimatedSpriteApp.h"
-#include <iostream>
-#include <exception>
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
+
 int main() {
 	
 	// allocation
 	auto app = new AnimatedSpriteApp();
-	try {
+	
 	// initialise and loop
 	app->run("AIE", 1280, 720, false);
-}
-catch (std::exception e) {
-	std::cout << e.what() << std::endl;
-}
-	// deallocation
-//delete app;
 
-	_CrtDumpMemoryLeaks();
+
+	// deallocation
+delete app;
+
+	
 	return 0;
 }
