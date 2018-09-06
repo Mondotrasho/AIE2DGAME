120446630e47d1acb57aafe911f4ba495c3fb16d
Mondotrasho <oscar@peoplepictures.com.au>
2018-09-06 21:52:10 +1000

    Fixed velocity by recalculating the direction more often in the Apply velocity instead of just in the controller

--- 2DGame/apply_velocity.h
+++ 2DGame/apply_velocity.h
@@ -3,24 +3,33 @@
 
 inline void apply_velocity(Grapple& Player,Vector2& velocity, float deltatime,float decay)
 {
-	
-	
+	Vector2 gravity = { 0,-200 };
+
+	auto perpindicular_dir = Vector2(-Player.get_ray().direction.y, Player.get_ray().direction.x).normalised();
 	Player.set_ray().origin += velocity * deltatime;
 	//decay
 	velocity -= (velocity * deltatime) / decay;
-
+	velocity.y += deltatime * gravity.y;
+	//velocity.y -= 200 * deltatime;
 	//move
-	auto perpindicular_dir = Vector2(Player.get_ray().direction.y, Player.get_ray().direction.x).normalised();
+	
 	//if grappled  and the length of the r
 	if (Player.state == 1)
 	{
-		auto distance = Player.get_ray().origin.distance(Player.intersect_point);
-		auto original_distance = Player.intercept_distance;
+		float distance = Player.get_ray().origin.distance(Player.intersect_point);
+		float original_distance = Player.intercept_distance;
+
+		Vector2 a = Player.intersect_point;
+		a = a - Player.get_ray().origin;
+		a.normalise();
+		Player.set_ray().direction = a;
+		
 
 		if (distance > original_distance)
 		{
 			velocity = perpindicular_dir * perpindicular_dir.dot(velocity);
-			Player.set_ray().origin = Player.intersect_point - (Player.get_ray().direction * Player.intercept_distance);
+			Player.set_ray().origin = Player.intersect_point - Player.get_ray().direction * Player.intercept_distance;
+
 		}
 	}
 };
